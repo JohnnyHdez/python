430db35302609fccb3b16b8429e76dd005f1a9c1
JohnnyHdez <juanito.h.r@gmail.com>
2024-09-29 22:10:43 -0500

    My anime database

--- anime_list/query.sql
+++ anime_list/query.sql
@@ -80,7 +80,7 @@ UPDATE emission SET name="Rurouni Kenshin: Meiji Kenkaku Romantan (2023)" WHERE 
 UPDATE emission SET season=3 WHERE id=3929
 UPDATE emission SET id=3917, next_chapter="2024-01-17" WHERE id=3971
 UPDATE emission SET state="Finalizado" WHERE id=4005
-UPDATE emission SET state="Finalizado",next_chapter="", last_chapter="2024-09-18" WHERE id=4005
+UPDATE emission SET state="Finalizado",next_chapter="", last_chapter="2024-09-27" WHERE id=3977
 UPDATE emission SET state="Finalizado", chapters=11, next_chapter="", last_chapter="2024-06-19" WHERE id=3988
 UPDATE emission SET id=3930 WHERE id=3970
 UPDATE emission SET year="2023-07-06", last_chapter="2023-12-14" WHERE id=3838
